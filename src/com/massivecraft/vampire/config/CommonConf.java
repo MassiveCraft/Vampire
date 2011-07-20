8b706e06a8cb66e8dcf95d982da1151546c05249
Olof Larsson <olof@sylt.nu>
2011-07-21 00:47:52 +0200

    Opps put it back

--- src/com/massivecraft/vampire/config/CommonConf.java
+++ src/com/massivecraft/vampire/config/CommonConf.java
@@ -14,6 +14,7 @@ public class CommonConf
 	
 	public static boolean allowSpreadingNecrosis = true;
 	public static int radiusSpreadingNecrosis = 2;
+	public static boolean burnInSunlight = true;
 	public static Recipe altarCureRecipe = new Recipe();
 	
 	static
--- src/com/massivecraft/vampire/config/TrueBloodConf.java
+++ src/com/massivecraft/vampire/config/TrueBloodConf.java
@@ -15,6 +15,7 @@ public class TrueBloodConf
 	
 	public static boolean allowSpreadingNecrosis = true;
 	public static int radiusSpreadingNecrosis = 2;
+	public static boolean burnInSunlight = true;
 	public static Recipe altarCureRecipe = new Recipe();
 	
 	static
